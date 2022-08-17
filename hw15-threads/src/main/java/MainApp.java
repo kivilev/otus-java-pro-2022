3444bc77e7c263063bfb2922194263c7be7ca4d7
d.kivilev <d.kivilev@qiwi.com>
2022-08-17 19:31:09 +0300

    Move to one class, optimization

--- hw15-threads/src/main/java/MainApp.java
+++ hw15-threads/src/main/java/MainApp.java
@@ -3,83 +3,92 @@ import org.slf4j.LoggerFactory;
 
 public class MainApp {
 
+    public static void main(String[] args) throws InterruptedException {
+        var calculationThread = new WorkThread(WorkMode.COUNT, "CalculationThread");
+        var displayThread = new WorkThread(WorkMode.DISPLAY, "DisplayThread");
+
+        calculationThread.start();
+        displayThread.start();
+
+        Thread.sleep(50000);
+
+        calculationThread.interrupt();
+        displayThread.interrupt();
+    }
+}
+
+class WorkThread extends Thread {
+
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 10;
 
     private static final Object WORK_MONITOR = new Object();
 
     private static int currentPosition = 0;
-    private static WorkMode workMode = WorkMode.GET_NEXT_COUNTER_VALUE;
     private static CountDirection countDirection = CountDirection.INCREMENT;
 
-    public static final Logger logger = LoggerFactory.getLogger(MainApp.class);
+    private static final Logger logger = LoggerFactory.getLogger(WorkThread.class);
 
-    public static void main(String[] args) {
-        Thread masterThread = new Thread(() -> {
-            var currentThread = Thread.currentThread();
-            currentThread.setName("MasterThread");
+    private final WorkMode threadWorkMode;
+    private static WorkMode currentWorkMode = WorkMode.COUNT;// за счет этого происходит выбор какой поток будет запущен первым
 
-            while (!currentThread.isInterrupted()) {
-                synchronized (WORK_MONITOR) {
-                    if (countDirection == CountDirection.INCREMENT) {
-                        currentPosition++;
-                        if (currentPosition == MAX_RANGE) countDirection = CountDirection.DECREMENT;
-                    } else if (countDirection == CountDirection.DECREMENT) {
-                        currentPosition--;
-                        if (currentPosition == MIN_RANGE) countDirection = CountDirection.INCREMENT;
-                    }
-
-                    logger.info("{}", currentPosition);
-
-                    workMode = WorkMode.SHOW_CURRENT_COUNTER_VALUE;
-                    WORK_MONITOR.notify();
-
-                    try {
-                        while (workMode != WorkMode.GET_NEXT_COUNTER_VALUE) {
-                            WORK_MONITOR.wait();
-                        }
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-                }
-            }
-        });
-
-        Thread slaveThread = new Thread(() -> {
-            var currentThread = Thread.currentThread();
-            currentThread.setName("SlaveThread");
-
-            while (!currentThread.isInterrupted()) {
-                synchronized (WORK_MONITOR) {
-                    while (workMode != WorkMode.SHOW_CURRENT_COUNTER_VALUE) {
-                        try {
-                            WORK_MONITOR.wait();
-                        } catch (InterruptedException e) {
-                            return;
-                        }
-                    }
-
-                    logger.info("{}", currentPosition);
-
-                    try {
-                        Thread.sleep(1000);// чтоб успевали прочесть сообщения
-                    } catch (InterruptedException e) {
-                        return;
-                    }
-
-                    workMode = WorkMode.GET_NEXT_COUNTER_VALUE;
-                    WORK_MONITOR.notify();
-                }
-            }
-        });
-
-        masterThread.start();
-        slaveThread.start();
+    public WorkThread(WorkMode threadWorkMode, String threadName) {
+        this.threadWorkMode = threadWorkMode;
+        setName(threadName);
     }
+
+    @Override
+    public void run() {
+        var currentThread = Thread.currentThread();
+
+        while (!currentThread.isInterrupted()) {
+            synchronized (WORK_MONITOR) {
+                while (threadWorkMode != currentWorkMode) {
+                    try {
+                        WORK_MONITOR.wait();
+                    } catch (InterruptedException e) {
+                        logger.info("Thread {} was interrupted", this.getName());
+                        return;
+                    }
+                }
+
+                if (currentWorkMode == WorkMode.COUNT) {
+                    calculate();
+                    currentWorkMode = WorkMode.DISPLAY;
+                } else {
+                    currentWorkMode = WorkMode.COUNT;
+                }
+
+                logger.info("Counter: {}", currentPosition);
+
+                WORK_MONITOR.notifyAll();
+
+                // для плавного отображения сообщений
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    logger.info("Thread {} was interrupted", this.getName());
+                    return;
+                }
+            }
+        }
+    }
+
+    private void calculate() {
+        if (countDirection == CountDirection.INCREMENT) {
+            currentPosition++;
+            if (currentPosition == MAX_RANGE) countDirection = CountDirection.DECREMENT;
+        } else if (countDirection == CountDirection.DECREMENT) {
+            currentPosition--;
+            if (currentPosition == MIN_RANGE) countDirection = CountDirection.INCREMENT;
+        }
+    }
+
 }
 
+
 enum WorkMode {
-    GET_NEXT_COUNTER_VALUE, SHOW_CURRENT_COUNTER_VALUE
+    COUNT, DISPLAY
 }
 
 enum CountDirection {
